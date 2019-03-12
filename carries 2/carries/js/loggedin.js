cc4f912b4e35e8ccb4a482eba6ec20a36e5bab9a
Shabib <ScionofGlory@hotmail.com>
2019-03-12 23:21:30 +0300

    login modifyyy

--- carries 2/carries/js/loggedin.js
+++ carries 2/carries/js/loggedin.js
@@ -1,4 +1,17 @@
 
+var logged;
+window.onload = function() {
+
+
+    if(logged == true) {
+        document.getElementById('authentic1').style.display = 'block';
+        document.getElementById('authentic').style.display = 'none';
+    } else {
+        document.getElementById('authentic').style.display = 'block';
+        document.getElementById('authentic1').style.display = 'none';
+    }
+}
+
 function logged() {
 
   logged = true;
