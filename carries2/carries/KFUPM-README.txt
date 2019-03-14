670b86d1d339e33881edb5d147485ac6a949b2a3
Shabib <ScionofGlory@hotmail.com>
2019-03-14 22:29:09 +0300

    Staff/Admin Login page + Sign up page added

---
+++ carries2/carries/KFUPM-README.txt
@@ -0,0 +1,5 @@
+Dear Grader,
+
+you can view the admin/staff page by going to E-Maintainance.atwebpages.com/vp-login.html
+
+Best Regards.
--- carries2/carries/js/indexloggedin.js
+++ carries2/carries/js/indexloggedin.js
@@ -4,5 +4,8 @@ window.onload = function() {
     if(localStorage.getItem('login') == 0) {
         document.getElementById('myorders').style.display = 'none';
         document.getElementById('myaccount').style.display = 'none';
+    } else {
+      document.getElementById('signup').style.display = 'none';
     }
+
 }
